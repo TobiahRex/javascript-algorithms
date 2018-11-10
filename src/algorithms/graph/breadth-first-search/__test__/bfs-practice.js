85d727503bd2b87589e139b0cd13e7884f4bd06e
Tobiah Rex <tobiah.rex@gmail.com>
2018-11-10 15:39:18 -0800

    finished advanced bfs practice.

--- src/algorithms/graph/breadth-first-search/__test__/bfs-practice.js
+++ src/algorithms/graph/breadth-first-search/__test__/bfs-practice.js
@@ -12,6 +12,40 @@ const bfs = (graph, startingVertex) => {
   }
 };
 
-function initCallback() {
-  
+const bfs_advanced = (graph, startingVertex, customCallbacks) => {
+  const cbs = initCallbacks(customCallbacks);
+  const vertexQueue = new Queue(startingVertex);
+  let previousVertex = null;
+
+  while (!vertexQueue.isEmpty()) {
+    const currentVertex = vertexQueue.dequeue();
+    cb.enteringNode({ currentVertex, previousVertex });
+    if (cb.allowTraversal(currentVertex)) {
+      graph.getNeighbors(currentVertex).forEach((vertex) => {
+        vertexQueue.enqueue(vertex);
+      });
+    }
+    cb.leavingNode({ currentVertex });
+    previousVertex = currentVertex;
+  }
+};
+
+function initCallback(cbs = {}) {
+  const initiatedCallbacks = cbs;
+  const stubCallback = () => {};
+
+  const allowTraversal = (() => {
+    const seen = {};
+    return ((vertex) => {
+      if (!(vertex.getKey() in seen)) {
+        seen[vertex.getKey()] = true;
+        return true;
+      }
+      return false;
+    });
+  })();
+
+  initiatedCallbacks.allowTraversal = cbs.allowTraversal || allowTraversal;
+  initiatedCallbacks.enterVertex = cbs.enterVertex || stubCallback;
+  initiatedCallbacks.leavingVertex = cbs.leavingVertex || stubCallback;
 }
